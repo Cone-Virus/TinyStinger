14d00441be88a275bdec8915c2f1c9199ad9848c
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-01-26 11:28:27 -0600

    Update setup.sh
    Fixed order due to perm issues

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 chmod +x scanner.sh
 git clone https://github.com/maurosoria/dirsearch.git
-sudo pip3 install -r dirsearch/requirements.txt
 git clone https://github.com/EnableSecurity/wafw00f
+sudo pip3 install -r dirsearch/requirements.txt
 cd wafw00f/
 sudo python3 setup.py build
 sudo python3 setup.py install
