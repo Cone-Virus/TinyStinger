3b8f306e996f5ade709b7714255979690e2016fa
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-01-26 00:28:20 -0600

    Update setup.sh
    Fixing the wafwoof builder section

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ chmod +x scanner.sh
 git clone https://github.com/maurosoria/dirsearch.git
 sudo pip3 install -r dirsearch/requirements.txt
 git clone https://github.com/EnableSecurity/wafw00f
-sudo python3 wafw00f/setup.py
+cd wafw00f/
+sudo python3 setup.py build
+sudo python3 setup.py install
+cd ..
