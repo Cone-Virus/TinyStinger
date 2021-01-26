ab348f53dc600ec8915c219096f7589fa4a5dcb2
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-01-26 09:08:40 -0600

    Update scanner.sh
    Overhaul update with a banner, help menu, file checker, and ability to use a custom wordlist

--- scanner.sh
+++ scanner.sh
@@ -1,16 +1,73 @@
 #!/bin/bash
 
 URLFILE=$1
+wordlist="dirsearch/db/dicc.txt"
+
+echo "
+▀█▀ █ █▄░█ █▄█   █▀ ▀█▀ █ █▄░█ █▀▀ █▀▀ █▀█
+░█░ █ █░▀█ ░█░   ▄█ ░█░ █ █░▀█ █▄█ ██▄ █▀▄
+Created by: @Cone_Virus
+                         (\\\\
+....-_...___-..-.._..-. -###)
+                          \"\"
+"
+
+function help_menu(){
+        echo "Please give a URL List
+Example: ./scanner.sh <URL List>
+
+Options:
+-w <Wordlist> : Use a custom wordlist in directory scanning"
+        exit 0
+}
+
+function file_check(){
+        if [ -f $1 ]; 
+        then
+                if [ ! -r $1 ]
+                then
+                        echo "Can't read the file $1"
+                        help_menu
+                fi
+        else
+                echo "$1 is not a valid $2"
+                help_menu
+        fi
+
+}
+
+if [[ $# == 0 ]] 
+then
+        help_menu
+fi
+
+args=("$@")
+count=0
+for arg in "$@"
+do
+        if [ "$arg" == "--help" ] || [ "$arg" == "-h" ] 
+        then
+                        help_menu
+                elif [ "$arg" == "-w" ]
+                then
+                        if [ "${args[$(($count + 1))]}" == "" ]
+                        then
+                                echo "No wordlist provided"
+                                help_menu
+                        else
+                                file_check "${args[$(($count + 1))]}" "wordlist"
+                                wordlist="${args[$(($count + 1))]}" 
+                        fi
+        fi
+        count=$(($count+1))
+done
+
+file_check "$URLFILE" "URL List"
+
+
 waflesstemp=$(mktemp WAFless-XXXXXX)
 waftemp=$(mktemp WAF-XXXXXX)
 dirtemp=$(mktemp DIR-XXXXXX)
-
-if [[ $1 == "" ]] 
-then
-echo "Please give a URL List"
-echo "Example: ./scanner.sh <URL List>"
-exit 0
-fi
 
 echo "Reading List"
 
@@ -38,7 +95,7 @@ echo "WAF Results"
 cat "WAF-Results"
 
 echo "Running Dirsearch on WAFLess"
-dirsearch/dirsearch.py -l $waflesstemp -e php,html,js,txt --plain-text=$dirtemp
+dirsearch/dirsearch.py -l $waflesstemp -w $wordlist -e php,html,js,txt --plain-text=$dirtemp
 
 echo "Ordering per URL"
 file=$(cat $waflesstemp)
