890cce886ded8e2cf69000a5946119778d0c7a8b
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-01-28 23:22:20 -0600

    Update setup.sh
    Added setup for Eel

--- setup.sh
+++ setup.sh
@@ -3,9 +3,14 @@
 chmod +x scanner.sh
 git clone https://github.com/maurosoria/dirsearch.git
 git clone https://github.com/EnableSecurity/wafw00f
+git clone https://github.com/ChrisKnott/Eel
 sudo pip3 install -r dirsearch/requirements.txt
 cd wafw00f/
 sudo python3 setup.py build
 sudo python3 setup.py install
-sudo apt-get install yad
+cd ..
+cd Eel/
+sudo python3 setup.py build
+sudo python3 setup.py install
+sudo pip3 install eel
 cd ..
