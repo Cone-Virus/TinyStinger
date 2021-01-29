8a45621ee873a8728c041b8115d88b4497c9a7a3
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-01-28 23:42:19 -0600

    Create StingerGUI.py
    A proper GUI first attempt

---
+++ StingerGUI.py
@@ -0,0 +1,32 @@
+import eel, os, json, sys
+
+#open and store database.json
+location = sys.argv[1]
+
+with open(f"{location}/Database.json", "r") as f:
+       data = json.load(f)
+ 
+loot = data['loot']
+
+#Return list of waf or wafless
+@eel.expose
+def waf_select(waf):
+    urls = []
+    for i in data[waf]:
+        urls.append(i['url'])
+
+    return urls
+
+#Return list of directory results
+@eel.expose
+def url_select(waf):
+    urls = []
+    for i in data[waf]:
+        temp = loot + '/' + i['dir']
+        urls.append(temp)
+
+    return urls
+    
+#Deploy Eel Server
+eel.init('BeeHive')
+eel.start('Stinger.html',mode='firefox') 
