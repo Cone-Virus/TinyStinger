a55361e6a0d0b7af4940954e6002fa0015df197a
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-02-02 14:14:49 -0600

    Update setup.sh
    Organizing Tools

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 chmod +x scanner.sh
-git clone https://github.com/maurosoria/dirsearch.git
-git clone https://github.com/EnableSecurity/wafw00f
-git clone https://github.com/ChrisKnott/Eel
-sudo pip3 install -r dirsearch/requirements.txt
-cd wafw00f/
+mkdir Tools
+git clone https://github.com/maurosoria/dirsearch.git Tools/dirsearch
+git clone https://github.com/EnableSecurity/wafw00f Tools/wafw00f
+git clone https://github.com/ChrisKnott/Eel Tools/Eel
+sudo pip3 install -r Tools/dirsearch/requirements.txt
+cd Tools/wafw00f/
 sudo python3 setup.py build
 sudo python3 setup.py install
-cd ..
-cd Eel/
+cd ../..
+cd Tools/Eel/
 sudo python3 setup.py build
 sudo python3 setup.py install
 sudo pip3 install eel
-cd ..
+cd ../..
