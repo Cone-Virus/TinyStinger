1aeca90451530482187931c4e77008c31421093f
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-02-02 20:44:46 -0600

    Update setup.sh
    remove dirsearch and add httprober and feroxbuster

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 
 chmod +x scanner.sh
 mkdir Tools
-git clone https://github.com/maurosoria/dirsearch.git Tools/dirsearch
+git clone https://github.com/tomnomnom/httprobe Tools/httprober
+curl -sL https://raw.githubusercontent.com/epi052/feroxbuster/master/install-nix.sh | bash
+mv feroxbuster Tools/
 git clone https://github.com/EnableSecurity/wafw00f Tools/wafw00f
 git clone https://github.com/ChrisKnott/Eel Tools/Eel
-sudo pip3 install -r Tools/dirsearch/requirements.txt
 cd Tools/wafw00f/
 sudo python3 setup.py build
 sudo python3 setup.py install
@@ -14,4 +15,9 @@ cd Tools/Eel/
 sudo python3 setup.py build
 sudo python3 setup.py install
 sudo pip3 install eel
+cd ../..
+sudo apt install -y golang
+cd Tools/httprober
+go build main.go
+mv main httprober
 cd ../..
