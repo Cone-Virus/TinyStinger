157227c7fc27db200f20ac3ad9e377cf4ef2e0b7
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2021-02-04 15:58:25 -0600

    Update utility.js
    Added selection for what results you wanna see (More results will be added in the future

--- BeeHive/utility.js
+++ BeeHive/utility.js
@@ -10,7 +10,7 @@ async function select_waf() {
     // Call into Python so we can access the file system
     
     // Build the form used to parse the json file. 
-    let wafForm = "<form onsubmit=\"pick_url(); return false;\" >\n";
+    let wafForm = "<form onsubmit=\"select_option(); return false;\" >\n";
     
     // Building the dropdown selector input. 
     wafForm += "<select name=\"URL\" id=\"url-select\">\n";
@@ -30,31 +30,69 @@ async function select_waf() {
     wafForm += "</form>\n";
 
     // Build a placeholder div for the URL search results. 
-    wafForm += "<div id=\"url-result\">---</div>\n";
+    wafForm += "<div id=\"option-results\">---</div>\n";
 
     // Put it all together.
     waf_results.innerHTML =  wafForm;
 }
 
+async function select_option() {
+    // Grab the options-results div to display list of options.
+    let optionForm = document.getElementById('option-results');
 
-async function pick_url() {
+    // Call into Python so we can access the file system
+
+    // Build the form used to parse the json file.
+    let optForm = "<form onsubmit=\"pick_results(); return false;\" >\n";
+
+    // Building the dropdown selector input.
+    optForm += "<select name=\"options\" id=\"select-options\">\n";
+
+    // Looping through the json file to get the first options.
+
+    optForm += '<option value= "dir">Directory Results</option>\n';
+    optForm += '<option value= "spid">Spider Results</option>\n';
+    optForm += '<option value= "osint">Osint Results</option>\n';
+    optForm += '<option value= "vuln">General Vulnerability Results</option>\n';
+
+    // Close out the Select dropdown.
+    optForm += "</select>\n";
+
+    // Make a submit button for the form.
+    optForm += "<button type=\"submit\">Select Results</button>\n";
+
+    // Close out the form.
+    optForm += "</form>\n";
+
+    // Build a placeholder div for the URL search results.
+    optForm += "<div id=\"pick-results\">---</div>\n";
+
+    // Put it all together.
+    optionForm.innerHTML =  optForm;
+}
+
+
+
+async function pick_results() {
     // Get the URL Value.
     let waf = document.getElementById('waf-select').value;
+    let opt = document.getElementById('select-options').value;
     let url = document.getElementById('url-select').value;
 
     // Get the URL container div. 
-    let url_div = document.getElementById('url-result');      
+    let pick_div = document.getElementById('pick-results');      
     
-    let url_results = ""
+    let pick_results = ""
     // Pull Back the URL information from the JSON. 
-    let filename = await eel.url_select(waf)();
+    let filename = await eel.url_select(waf, opt)();
 
     // Loop through and parse the directory results. 
     //for (let i in filename) 
     //{
-        url_results += '<p><iframe src=\"' + filename[url] + '\" frameborder=\"0\" height=\"400\" width=\"95%\"></iframe></p>'
+        pick_results += '<iframe class=\"show\" src=\"' + filename[url] + '\" align=\"right\" ></iframe>'
     //}
 
     // Put the results in the container div. 
-    url_div.innerHTML =  url_results;
+    pick_div.innerHTML =  pick_results;
 }
+      
